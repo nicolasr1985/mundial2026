f4decd957b7ecb172c4b3909d8f9432603c83b14
Nicolas Rodriguez <nicolasr9@gmail.com>
2026-07-11 22:50:26 -0500

    chore: Ndoye 2, Alvarez/MacAllister 1, Argentina FIFA to #2

--- lib/fifa-ranking.ts
+++ lib/fifa-ranking.ts
@@ -7,8 +7,8 @@ export interface FifaEntry {
 
 export const FIFA_RANKINGS: FifaEntry[] = [
   { rank: 1, name: "France", code: "FRA", pts: 1948.97 },
-  { rank: 2, name: "Spain", code: "ESP", pts: 1934.79 },
-  { rank: 3, name: "Argentina", code: "ARG", pts: 1925.15 },
+  { rank: 2, name: "Argentina", code: "ARG", pts: 1943.47 },
+  { rank: 3, name: "Spain", code: "ESP", pts: 1934.79 },
   { rank: 4, name: "England", code: "ENG", pts: 1889.42 },
   { rank: 5, name: "Brazil", code: "BRA", pts: 1804.92 },
   { rank: 6, name: "Morocco", code: "MAR", pts: 1803.99 },
